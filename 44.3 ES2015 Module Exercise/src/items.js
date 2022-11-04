f9e19b5805b3270febc93e963322f3b8c81e2e45
Daniel Jordan <djordan218@gmail.com>
2022-11-04 11:18:41 -0500

    added 44.3 exercise

---
+++ 44.3 ES2015 Module Exercise/src/items.js
@@ -0,0 +1,32 @@
+const items = [
+  {
+    id: 1,
+    name: 'Gatorade G Zero Thirst Quencher, 3 Flavor Variety Pack',
+    price: 12.82,
+    qty: 8,
+    img: 'https://m.media-amazon.com/images/I/81MARvbTP-S._SL1500_.jpg',
+  },
+  {
+    id: 2,
+    name: 'BLACK+DECKER 20V Max Handheld Vacuum, Cordless',
+    price: 79.99,
+    qty: 1,
+    img: 'https://m.media-amazon.com/images/I/516LsONVRuS._AC_SL1000_.jpg',
+  },
+  {
+    id: 3,
+    name: '4-7/8" Zinc Plated Strike Plate (Large Hole)',
+    price: 12.99,
+    qty: 1,
+    img: 'https://m.media-amazon.com/images/I/31GcYA1XaQL._AC_.jpg',
+  },
+  {
+    id: 4,
+    name: 'Power One Mercury Free Hearing Aid Batteries Size P312',
+    price: 12.49,
+    qty: 5,
+    img: 'https://m.media-amazon.com/images/I/61njFqGvnWL._AC_SL1128_.jpg',
+  },
+];
+
+export default items;
